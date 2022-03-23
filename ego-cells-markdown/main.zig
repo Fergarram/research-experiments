38757644c61fba5eced48bda0bb476c25302b0a9
Fernando Garcia <fergarram@gmail.com>
2022-03-22 20:55:33 -0600

    ego-cells-markdown: cleaned up code, added sample format for JSON file and sample server program.

--- ego-cells-markdown/main.zig
+++ ego-cells-markdown/main.zig
@@ -1,42 +1,19 @@
-//   ABCDEFGHIJKLMNOP //
-// A ---------------- //
-// B # Title--------- //
-// C ---------------- //
-// D Paragraph text-- //
-// E ---------------- //
-
-// In this grid most cells are unactivated or represent an empty cell but there are a few that have been given single values.
-
-// Each cell can access their neighbors and based on that they can assume an identity, in the case of cell AB it's 100% certain it's a '#' character and it would be X% sure that it's part of a "heading 1" bigger holon because this cell has knowledge about what it can be a part of and what it takes to be a part of such a bigger holon based on its vision of its neighbors' value and (maybe) their assumptions about what they can be in the bigger scope.
-
-// There might be a need for a second or more layers of cells that help with the bigger scopes. This might be needed for parallelism — supervision of a range of cells might execute faster when in parallel or it simply might not be possible otherwise.
-
-// Maybe the first layer is static and thus it feels weird to only have "one" layer of cells but in reality it's two but one is static because the value is a static character.
-
-// So what I need to do is create a representation of an ego cell.
-
-// So, something like a dynamic "union" type that can become a value of 0 - 100% of an identity.
-
-// The tendency to identify with a block will be defined by each cell's neighbors. 
-
 const std = @import("std");
 const print = std.debug.print;
 
-// All in-line blocks are full-line blocks but not all full-line blocks are in-line blocks.
 const Block = enum {
-    H1, H2, H3, H4, H5, H6, // full-line 
-    PARAGRAPH, // full-line
-    UL_ITEM, // full-line
-    OL_ITEM, // full-line
-    MONO, // in-line
-    UNDERLINE, // in-line
-    BOLD, // in-line
-    ITALIC, // in-line
-    STRIKE, // in-line
-    LINK, // in-line
-    CODE_SNIPPET, // multi-line
-    BLOCKQUOTE, // multi-line
-
+    H1, H2, H3, H4, H5, H6,
+    PARAGRAPH,
+    UL_ITEM,
+    OL_ITEM,
+    MONO,
+    UNDERLINE,
+    BOLD,
+    ITALIC,
+    STRIKE,
+    LINK,
+    CODE_SNIPPET,
+    BLOCKQUOTE,
     // NOTE: Tables are not part of MD but would be interesting
     //       https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#tables
 };
@@ -54,10 +31,10 @@ const Cell = struct {
     x: i32,
     y: i32,
 
-    value: ?u8 = null,
+    value: u8 = undefined,
 
     // For each Block type a cell can have a % of identification with being part of the block. The order of the items in the array represents the block type, so the first item would point to the H1 block.
-    tendencies: [@typeInfo(Block).Enum.fields.len] f16 = undefined,
+    tendencies: [@typeInfo(Block).Enum.fields.len] f16 = [_]f16{0} ** @typeInfo(Block).Enum.fields.len,
 
     // 0 1 2
     // 7   3
@@ -116,6 +93,15 @@ pub fn main() !void {
 
     for (cellPtrList.items) |cellPtr| {
         if (cellPtr.* != null) {
+            const topleft = getCellPtr(cellPtr.*.?.neighbors[0]);
+            const top = getCellPtr(cellPtr.*.?.neighbors[1]);
+            const topright = getCellPtr(cellPtr.*.?.neighbors[2]);
+            const right = getCellPtr(cellPtr.*.?.neighbors[3]);
+            const bottomright = getCellPtr(cellPtr.*.?.neighbors[4]);
+            const bottom = getCellPtr(cellPtr.*.?.neighbors[5]);
+            const bottomleft = getCellPtr(cellPtr.*.?.neighbors[6]);
+            const left = getCellPtr(cellPtr.*.?.neighbors[7]);
+
             // Rules for being a '#':
             // 1. If I'm a '#' and I'm at the beginning, I could be a heading start.
             // 2. If my right neighbor is a space, I am a heading 1 start.
@@ -125,33 +111,71 @@ pub fn main() !void {
             //    me that I'm a heading as well.
 
             // NOTES:
-            // * How much does it increase in activation which each rule?
-            // * Could it be that the amounts of rules and their conclusions define 
+            // * How much does it increase in activation with each rule?
+            // * Could it be that the amount of rules and their conclusions define 
             //   the amount of activation it will have?
             // * How do rules cancel each other out?
+            // * Can there be an algorithm that defines the amount of activation a set of 
+            //   rules would result in?
 
-            print("{u}", .{cellPtr.*.?.value});
+            if (cellPtr.*.?.value == '#') {
+                cellPtr.*.?.tendencies[0] = 0.25;
+                cellPtr.*.?.tendencies[1] = 0.25;
+                cellPtr.*.?.tendencies[2] = 0.25;
+                cellPtr.*.?.tendencies[3] = 0.25;
+                cellPtr.*.?.tendencies[4] = 0.25;
+                cellPtr.*.?.tendencies[5] = 0.25;
+            }
+
+            if (right.* != null and right.*.?.value == ' ') {
+                cellPtr.*.?.tendencies[0] = 0.9;
+                cellPtr.*.?.tendencies[1] = 0;
+                cellPtr.*.?.tendencies[2] = 0;
+                cellPtr.*.?.tendencies[3] = 0;
+                cellPtr.*.?.tendencies[4] = 0;
+                cellPtr.*.?.tendencies[5] = 0;
+            }
         }
+
+        // @TODO: Export a JSON file with the state of each cell so that I can analyze it and visualize it in each generation or step.
+    
+        // Extra points: Create a websocket endpoint that serves this JSON: 
+        // developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers
     }
 
-    // @TODO: Export a JSON file with the state of each cell so that I can analyze it and visualize it. Extra points: Create a REST endpoint that serves this JSON.
+    //
+    // Loop to query each cell in terminal
+    //
 
-    // const sx: usize = 14;
-    // const sy: usize = 3;
+    const stdin = std.io.getStdIn().reader();
 
-    // const cell: *?Cell = &cellMatrix[sx][sy];
+    while (true) {
+        var sx: usize = 0;
+        var sy: usize = 0;
+        var readBuffer: [10]u8 = undefined;
 
-    // if (cell.* != null) {
-    //     print("Character for cell in ({d}, {d}): '{u}'\n", .{ sx, sy, cell.*.?.value });
+        print("X: ", .{});
+        if (try stdin.readUntilDelimiterOrEof(readBuffer[0..], '\n')) |user_input| {
+            sx = try std.fmt.parseInt(usize, user_input, 10);
+        }
 
-    //     const neighborCell: *?Cell = getCellPtr(cell.*.?.neighbors[3]);
-    //     if (neighborCell.*) |n| {
-    //         print("Right neighbor character: '{u}'\n", .{n.value});
-    //     } else {
-    //         print("Right neighbor character: null\n", .{});
-    //     }
+        print("Y: ", .{});
+        if (try stdin.readUntilDelimiterOrEof(readBuffer[0..], '\n')) |user_input| {
+            sy = try std.fmt.parseInt(usize, user_input, 10);
+        }
 
-    // } else {
-    //     print("Character for cell in ({d}, {d}): null\n", .{ sx, sy });
-    // }
+        const cell: *?Cell = &cellMatrix[sx][sy];
+
+        if (cell.* != null) {
+            print("Value for ({d}, {d}): '{u}'\n", .{ sx, sy, cell.*.?.value });
+            for (cell.*.?.tendencies) |tendency, index| {
+                print("Tendency[{}]: {d:.3}\n", .{ index, tendency});
+            }
+
+        } else {
+            print("Null cell at ({d}, {d})\n", .{ sx, sy });
+        }
+
+        print("\n\n", .{}); 
+    }
 }
